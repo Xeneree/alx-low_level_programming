5b36a1c27e58f1749e5dedfe5700d18e9988a240
xeneree <xeneree@gmail.com>
2022-09-13 15:49:35 -0700

    Task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * print_alphabet_x10 - function to print abc 10 times
  *
  * Return: 0
  */
+
 void print_alphabet_x10(void)
 {
 	char c, i;
@@ -16,3 +18,4 @@ void print_alphabet_x10(void)
 		}
 		_putchar('\n');
 	}
+}
